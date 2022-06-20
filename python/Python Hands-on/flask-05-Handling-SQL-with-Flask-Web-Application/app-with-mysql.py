267c445795c055483b8ed9c4ea29efd9c76fdaed
Vedat Kocoglu <88839202+Vedooo@users.noreply.github.com>
2022-06-20 23:04:09 +0300

    Update app-with-mysql.py

--- python/Python Hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ python/Python Hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_HOST'] = 'vedodb.ccl8yxbfrine.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'vedovedo1'
-app.config['MYSQL_DATABASE_DB'] = 'clarusway'
+app.config['MYSQL_DATABASE_DB'] = 'expdb'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 mysql = MySQL()
 mysql.init_app(app)
